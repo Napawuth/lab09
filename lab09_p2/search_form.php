0c87a42c0b0f8d693b02b633bd147f163089bd80
Napawuth Homsa-ard <104339012@student.swin.edu.au>
2026-05-13 14:55:31 +1000

    complete part 2

---
+++ lab09_p2/search_form.php
@@ -0,0 +1,6 @@
+<?php
+    echo '<form action="search_result.php" method="GET">';
+    echo '<input type="text" name="model" placeholder="Search for a car model...">';
+    echo '<button type="submit">Search</button>';
+    echo '</form>';
+?>
