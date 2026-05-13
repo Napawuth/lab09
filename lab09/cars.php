acb7e2dd2a649729504aed3ba52be90196cda0eb
Napawuth Homsa-ard <104339012@student.swin.edu.au>
2026-05-13 13:08:51 +1000

    Fix lab09 tracking and add files

---
+++ lab09/cars.php
@@ -0,0 +1,30 @@
+<?php
+    require_once "setting.php";
+    $dbconn = @mysqli_connect($host,$user, $pwd, $sql_db);
+    if($dbconn){
+        $query = "SELECT * FROM cars";
+        $result = mysqli_query($dbconn,$query);
+        if($result){
+            echo "<table border='1'>";
+            echo "<tr>
+                    <th>ID</th>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Price</th>
+                    <th>YOM</th>
+                  </tr>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['car_id'] . "</td>";
+                echo "<td>" . $row['make'] . "</td>";
+                echo "<td>" . $row['model'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row['yom'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
+        else{}
+        mysqli_close($dbconn);
+    } else echo "<p>Unable to connect to the db.</p>";
+?>
